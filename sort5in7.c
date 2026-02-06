601e3d200d1962bd33c013dcbc0ba905041d4d3e
Stew Forster <stew675@gmail.com>
2026-02-06 12:34:38 -0600

    Cleanup and refactor to use less swaps

--- sort5in7.c
+++ sort5in7.c
@@ -25,145 +25,150 @@
 #include <stdbool.h>
 #include <assert.h>
 
-#define MAX_PERMUTE     20      // Maximum number of items we will permute
 #define TEST_SIZE       5       // Number of items in array we're sorting
 
-static size_t   num_comps = 0, num_swaps = 0;
+static size_t   start_comps = 0, num_comps = 0, num_swaps = 0;
 static double   num_sorts = 0;
-static void     swap(void *, void *);
-static bool     is_lt(void *, void *);          // True if first is less than second
+
+// Returns boolean result of *pa < *pb.  Counts comparisons
+static bool
+is_lt(int *pa, int *pb)
+{
+	num_comps++;
+	return (*pa < *pb);
+} // is_lt
+
+// Swaps two integers pointed at by *pa and *pb.  Counts swaps
+static void
+swap(int *pa, int *pb)
+{
+	num_swaps++;
+	int t = *pa;
+	*pa = *pb;
+	*pb = t;
+} // swap
 
 
 // Sorts 5 items in at most 7 comparisons (or less)
+// This solution also averages just 4.6 swaps by centralising unsorted elements
 static void
 sort5in7(int *p1, int *p2, int *p3, int *p4, int *p5)
 {
-        bool p1_was_moved = false;
+	bool p1_was_moved = false;
 
-        // Sort p1 and p2
-        if (is_lt(p2, p1))
-                swap(p1, p2);   // (p1, p2) | p3 | p4 | p5
+	// Sort p1 and p2
+	if (is_lt(p2, p1))
+		swap(p1, p2);   // (p1, p2) | p3 | p4 | p5
 
-        // Sort p3 and p4
-        if (is_lt(p4, p3))
-                swap(p3, p4);   // (p1, p2) | (p3, p4) | p5
+	// Sort p4 and p5
+	if (is_lt(p5, p4))
+		swap(p4, p5);   // (p1, p2) | p3 | (p4, p5)
 
-        // Establish a set of three sorted items at positions
-        // P1, P3, and P4.  We'll handle placing P2 later on
-        if (is_lt(p3, p1)) {
-                swap(p1, p3);
-                swap(p2, p4);
-        }
+	// Establish a set of three sorted items at positions
+	// P1, P4, and P5.  We'll handle placing P2 later on
+	if (is_lt(p4, p1)) {
+		swap(p1, p4);
+		swap(p2, p5);
+	}
 
-        // After the above, we know: P1 < P3 < P4, AND P1 < P2
-        // Note that we do NOT know if P2 < P3 yet
+	// After the above, we know: P1 <= P4 <= P5, AND P1 <= P2
+	// Note that we do NOT know if P2 <= P4 or if P2 <= P5 yet
+	// Where P3 must go is not known at all at this time
 
-        // Now place P5 in (P1,P3,P4) with a binary search.
-        // This will take two comparisons.
-        if (is_lt(p5, p3)) {
-                // Take note of if P5 < P1, as this will move
-                // P1 past P2 into P3, and we can save on a
-                // comparison later if we record this result
-                if ((p1_was_moved = is_lt(p5, p1)))
-                        swap(p1, p5);
-                swap(p3, p5);
-                swap(p4, p5);
-        } else {
-                if (is_lt(p5, p4))
-                        swap(p4, p5);
-        }
+	// Now place P3 in order with (P1,P4,P5) using a binary search.
+	// This will always take two comparisons.
+	if (is_lt(p4, p3)) {
+		swap(p3, p4);
+		if (is_lt(p5, p4))
+			swap(p4, p5);
+	} else {
+		// Take note of if P3 < P1, as this will move
+		// P1 past P2 into P3, and we can save on a
+		// comparison later if we record this result
+		if ((p1_was_moved = is_lt(p3, p1)))
+			swap(p1, p3);
+	}
 
-        // We now have:  P1 < P3 < P4 < P5, AND P1 < P2.  Place
-        // P2 into P3/P4/P5 using a binary search.  This can use
-        // less than two comparisons if p1_was_moved is true
-        if (is_lt(p4, p2)) {
-                // Move P2 up to P4
-                swap(p2, p4);
-                swap(p2, p3);
-                // Now test if we need to move P4 to P5
-                if (is_lt(p5, p4))
-                        swap(p4, p5);
-        } else {
-                // If P1 moved to P3, (and P3 moved to P4) and
-                // since we checked P2 < P4 above, we can skip
-                // a comparison here under that circumstance
-                if (p1_was_moved || is_lt(p3, p2))
-                        swap(p2, p3);
-        }
+	// We now know that:  P1 <= P3 <= P4 <= P5
+	// If P1 was moved to P3 above, then P2 and P3 must be swapped
+	// If P1 was not moved to P3, then we also know that P1 <= P2
+	// Now place P2 into P3/P4/P5 using a binary search.
+	if (is_lt(p4, p2)) {
+		// Move P2 up to P4
+		swap(p2, p3);	// Also satisfies the p1_was_moved scenario
+		swap(p3, p4);
+		// Now test if we need to move P4 to P5
+		if (is_lt(p5, p4))
+			swap(p4, p5);
+	} else {
+		// If P1 had moved to P3 then we can just swap P2 and P3
+		// Otherwise we need to check if P3 < P2 before swapping
+		if (p1_was_moved || is_lt(p3, p2))
+			swap(p2, p3);
+	}
 } // sort5in7
-
-
-static bool
-is_lt(void *va, void *vb)
-{
-        num_comps++;
-        return (*(int *)va < *(int *)vb);
-} // is_lt
-
-static void
-swap(void *va, void *vb)
-{
-        num_swaps++;
-        int t = *(int *)va;
-        *(int *)va = *(int *)vb;
-        *(int *)vb = t;
-} // swap
 
 static void
 print_array(int *a, size_t n)
 {
-        printf("[ %2d", a[0]);
-        for (size_t i = 1; i < n - 1; i++)
-                printf(", %2d", a[i]);
-        printf(", %2d]\n", a[n - 1]);
+	printf("[ %2d", a[0]);
+	for (size_t i = 1; i < n - 1; i++)
+		printf(", %2d", a[i]);
+	printf(", %2d]\n", a[n - 1]);
 } // print_array
 
 static void
 validate(int *a, int *t, size_t n)
 {
-        size_t  i;
+	bool	print_io = false;
 
-        for (i = n - 1; i > 0; i--)
-                if (t[i] < t[i-1])
-                        break;
+	// Check if sort exceeded 7 comparisons
+	size_t	nc = num_comps - start_comps;
+	if (nc > 7) {
+		printf("\nSORT USED GREATER THAN 7 COMPARISONS: ");
+		printf("%lu compares\n", nc);
+		print_io = true;
+	}
 
-        if (i) {
-                printf("\nSORT FAILED\n");
-                printf("INPUT ARRAY  -> ");
-                print_array(a, n);
-                printf("OUTPUT ARRAY -> ");
-                print_array(t, n);
-                printf("\n");
-        }
+	// Check if sorted items are out of order
+	size_t	i;
+	for (i = n - 1; i > 0; i--)
+		if (t[i] < t[i-1])
+			break;
+
+	if (i) {
+		printf("\nSORT FAILED\n");
+		print_io = true;
+	}
+
+	// Print input and output arrays if required
+	if (print_io) {
+		printf("INPUT ARRAY  -> ");
+		print_array(a, n);
+		printf("OUTPUT ARRAY -> ");
+		print_array(t, n);
+		printf("\n");
+	}
 } // validate
-
-// Permutation helper that doesn't count swaps
-static inline void
-pswap(void *va, void *vb)
-{
-        int t = *(int *)va;
-        *(int *)va = *(int *)vb;
-        *(int *)vb = t;
-} // pswap
 
 static void
 permute(int a[], int pos, int n, const void (*_func)(void *, const int))
 {
-        assert(pos < MAX_PERMUTE);
+	assert(pos < TEST_SIZE);
 
-        if (pos >= (n - 1))
-                return _func(a, n);
+	if (pos >= (n - 1))
+		return _func(a, n);
 
-        for (int i = pos; i < n; i++) {
-                // Swap current element
-                pswap(&a[pos], &a[i]);
+	for (int i = pos, t; i < n; i++) {
+		// Swap current element
+		t = a[pos]; a[pos] = a[i]; a[i] = t;
 
-                // Recurse to permute the rest of the array
-                permute(a, pos + 1, n, _func);
+		// Recurse to permute the rest of the array
+		permute(a, pos + 1, n, _func);
 
-                // Undo what we swapped
-                pswap(&a[pos], &a[i]);
-        }
+		// Undo what we swapped
+		t = a[pos]; a[pos] = a[i]; a[i] = t;
+	}
 } // permute
 
 // Receives an array permutation and invokes the actual
@@ -171,35 +176,38 @@ permute(int a[], int pos, int n, const void (*_func)(void *, const int))
 static void
 call_sort(void *va, const int n)
 {
-        int *a = (int *)va;
-        int cpa[MAX_PERMUTE];   // Holds copy of items to sort
+	int *a = (int *)va;
+	int cpa[TEST_SIZE];   // Holds copy of items to sort
 
-        // Copy the permuted array before sorting as we do
-        // not want to sort what permute() is working with
-        for (size_t i = 0; i < n; i++) cpa[i] = a[i];
+	// Copy the permuted array before sorting as we do
+	// not want to sort what permute() is working with
+	for (size_t i = 0; i < n; i++) cpa[i] = a[i];
 
-        // Call our sort function with the (copied) permutation
-        sort5in7(&cpa[0], &cpa[1], &cpa[2], &cpa[3], &cpa[4]);
+	// Save current comparisons counter
+	start_comps = num_comps;
 
-        // Validate that it's correct
-        validate(a, cpa, n);
+	// Call our sort function with the (copied) permutation
+	sort5in7(&cpa[0], &cpa[1], &cpa[2], &cpa[3], &cpa[4]);
 
-        num_sorts++;
+	// Validate that it's correct
+	validate(a, cpa, n);
+
+	num_sorts++;
 } // do_sort
 
 int
 main()
 {
-        int     p[TEST_SIZE] = {0};
+	int     p[TEST_SIZE] = {0};
 
-        printf("\nTest Sort Size is %d items\n\n", (int)TEST_SIZE);
+	printf("\nTest Sort Size is %d items\n\n", (int)TEST_SIZE);
 
-        // Initialise the permute test array
-        for (int i = 0; i < TEST_SIZE; i++)
-                p[i] = i + 1;
+	// Initialise the permute test array
+	for (int i = 0; i < TEST_SIZE; i++)
+		p[i] = i + 1;
 
-        permute(p, 0, TEST_SIZE, call_sort);
+	permute(p, 0, TEST_SIZE, call_sort);
 
-        printf("num_sorts = %7.0f,  avg. comps = %7.3f,  avg. swaps = %7.3f\n\n",
-                num_sorts, num_comps/num_sorts, num_swaps/num_sorts);
+	printf("num_sorts = %7.0f,  avg. comps = %7.3f,  avg. swaps = %7.3f\n\n",
+		num_sorts, num_comps/num_sorts, num_swaps/num_sorts);
 } // main
